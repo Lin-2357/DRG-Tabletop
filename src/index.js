ba38da63ddbef8985ad36554fbbde4d3dee92bb0
Jiayin Lin <linjiayin66@yahoo.com>
2024-08-23 23:03:29 -0700

    sd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
-import Demo from './github/Demo';
+import Demo from './github/Demo.js';
 
 // ========================================
 
